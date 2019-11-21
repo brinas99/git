e300cf2e870e82ef19d48571d8067bec07c9d03a
brinas99 <brinitas1999@hotmail.com>
2019-11-21 21:08:44 +0100

    script compara parametros

---
+++ scriptcondiciones.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ $1 -gt 4000 ]
+then
+	echo "El numero es mayor que 4000"
+else
+	echo "El numero es menor que 4000"
+fi
+exit 0
--- scriptldifpiddatos.sh
+++ scriptldifpiddatos.sh
@@ -4,24 +4,40 @@ let ou
 let var_1dc
 let var_2dc
 let sn
+uidNumber=1
+gidNumber=1
 read -p "Nombre: " uid
 read -p "Apellido: " sn
 read -p "Unidad organizativa: " ou
 read -p "Nombre de dominio: " var_1dc
 read -p "Exatension de dominio: " var_2dc
-classObject1=inetOrgPerson
-classObject2=posixAccount
-classObject3=person
+objectClass1=inetOrgPerson
+objectClass2=posixAccount
+objectClass3=person
 primeralinea=uid=$uid,ou=$ou,dc=$var_1dc,dc=$var_2dc
 echo dn: $primeralinea
-echo classObject: $classObject1
-echo classObject: $classObject2
-echo classObject: $classObject3
+echo objectClass: $objectClass1
+echo objectClass: $objectClass2
+echo objectClass: $objectClass3
 echo uid: $uid
+echo cn: $uid
 echo sn: $sn
+echo givenName: $uid
+echo userPassword: $uid
+echo loginShell: /bin/bash
+echo uidNumber: $uidNumber
+echo gidNumber: $gidNumber
+echo homeDirectory: /home/$uid
 echo dn: $primeralinea > salida2.ldif
-echo classObject: $classObject1 >> salida2.ldif
-echo classObject: $classObject2 >> salida2.ldif
-echo classObject: $classObject3 >> salida2.ldif
+echo objectClass: $objectClass1 >> salida2.ldif
+echo objectClass: $objectClass2 >> salida2.ldif
+echo objectClass: $objectClass3 >> salida2.ldif
 echo uid: $uid >> salida2.ldif
+echo cn: $uid >> salida2.ldif
 echo sn: $sn >> salida2.ldif
+echo givenName: $uid >> salida2.ldif
+echo userPassword: $uid >> salida2.ldif
+echo loginShell: /bin/bash >> salida2.ldif
+echo uidNumber: $uidNumber >> salida2.ldif
+echo gidNumber: $gidNumber >> salida2.ldif
+echo homeDirectory: /home/$uid >> salida2.ldif
